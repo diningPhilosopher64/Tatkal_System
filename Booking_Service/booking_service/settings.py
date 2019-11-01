0ab7a3c69313906cb4766c26fd6b49a776d7f300
diningPhilosopher64 <sourabhkondapaka@gmail.com>
2019-11-01 16:16:27 +0530

    Changed django settings in both the servers to use the postgres db

--- Booking_Service/booking_service/settings.py
+++ Booking_Service/booking_service/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'booking_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
 
